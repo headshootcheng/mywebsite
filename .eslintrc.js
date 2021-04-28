e9d84a55e40fd3fed55e3e7bae6ae66f80d05550
petercheng <petercheng7788@gmail.com>
2021-04-29 02:59:50 +0800

    added typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,28 @@ module.exports = {
     ecmaVersion: 2020,
     ecmaFeatures: { jsx: true },
     sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
   },
-  plugins: ["prettier", "unused-imports"], // use plugin
+  plugins: ["prettier", "unused-imports", "@typescript-eslint"], // use plugin
   extends: [
-    "plugin:react/recommended", // as prettier has different set of rule,
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended", // as prettier has different set of rule,
     "plugin:prettier/recommended", // we use the recommended set
   ],
   rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "warn", // : any
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-inferrable-types": "off", // allow typing default variables
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off", // Using require('...')
     "react/react-in-jsx-scope": "off", // must include import from react, otherwise show error
     "prettier/prettier": "warn", // styling warning based on prettier
     "react/prop-types": "off",
